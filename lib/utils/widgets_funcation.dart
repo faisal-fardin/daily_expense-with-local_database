f2d912e4b146f1f60fd8694196b23e32a409de9a
Faisal <faisalfardin77@gmail.com>
2023-12-30 23:24:36 +0600

    added something design

--- lib/utils/widgets_funcation.dart
+++ lib/utils/widgets_funcation.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 showMsg(BuildContext context, String msg) =>
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)),);
-
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(msg)),
+    );
 
 showSingleTextFieldDialog({
   required BuildContext context,
@@ -18,15 +19,27 @@ showSingleTextFieldDialog({
   showDialog(
       barrierDismissible: false,
       context: context,
-      builder: (context) =>
-          AlertDialog(
-            title: Text(title),
+      builder: (context) => AlertDialog(
+            title: Text(title,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold)),
             content: Padding(
               padding: const EdgeInsets.all(8),
               child: TextField(
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
                 controller: controller,
                 keyboardType: inputType,
-                decoration: InputDecoration(hintText: hint),
+                decoration: InputDecoration(
+                    hintText: hint,
+                    hintStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold)),
               ),
             ),
             actions: [
@@ -34,9 +47,16 @@ showSingleTextFieldDialog({
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: Text(negativeBtnText),
+                child: Text(negativeBtnText,
+                    style: const TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)),
               ),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple
+                ),
                 onPressed: () {
                   if (controller.text.isEmpty) {
                     return;
@@ -44,7 +64,13 @@ showSingleTextFieldDialog({
                   Navigator.pop(context);
                   onSave(controller.text);
                 },
-                child: Text(positiveBtnText),
+                child: Text(
+                  positiveBtnText,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ],
           ));
